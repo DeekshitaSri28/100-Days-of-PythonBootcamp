ccd472785204356413a975c7088ef919d86069db
DeekshitaSri28 <pentadeekshitasri@gmail.com>
2026-07-02 19:00:25 +0530

    Initial commit

---
+++ Band Name Generator Project/main.py
@@ -0,0 +1,4 @@
+print("Welcome To The Band Name Generator")
+City = input("What is Your City name?")
+Pet = input("What is your pet Name?")
+print(City+" "+ Pet)
---
+++ task/task.py
@@ -0,0 +1,3 @@
+# Write your code below this line 👇
+print("Hello world!\nHello World!\nHello World!")
+print("Hello"+" Angela")
